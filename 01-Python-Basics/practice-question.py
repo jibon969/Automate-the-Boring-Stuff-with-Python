dffedecd2eb6b5219eb58636d92cd00389d6d9ef
= <=>
2021-11-02 00:07:46 +0600

    added basic-python practics

---
+++ 01-Python-Basics/practice-question.py
@@ -0,0 +1,33 @@
+
+#1. Which of the folling are operators, and which are values ?
+"""
+*
+'hello'
+-88.8
+-
+/
++
+5
+"""
+# Answer : 'hello', -88.8, 5 are values
+# *, -, /, + are operators
+
+#2. which of the following is a variable and which is a string?
+"""
+spam
+'spam'
+"""
+spam = 'spam'
+print(spam)
+
+#3. Name three data types ?
+# Answer : int, float, string
+
+#4. What does the variable bacon contain after the folling code runs ?
+
+#5. What should the folling two expressions evaluate to ?
+x = 'spam' + 'spamspam'
+print(x)
+
+y = 'spam' * 3
+print(y)
